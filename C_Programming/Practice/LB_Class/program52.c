8a95fe5b52c0614680b1bb394805771be0064ef4
shindeomkar0069 <shindeomkar0069@gmail.com>
2026-06-19 11:21:07 +0530

    Add files via upload

---
+++ C_Programming/Practice/LB_Class/program52.c
@@ -0,0 +1,27 @@
+//Input :5
+//Output : 1 2 3 4 5
+
+#include<stdio.h>
+void Display(int iNo)
+{
+  int iCnt =0;
+
+  for(iCnt=1 ; iCnt<=iNo; iCnt++)
+  {
+    printf("%d\t");
+  }
+  
+
+}
+int main()
+{
+   int iValue = 0;
+   printf("Enter Number :\n");
+   scanf("%d",&iValue);
+
+   Display(iValue);
+
+
+    return 0;
+    
+}
---
+++ C_Programming/Practice/LB_Class/program53.c
@@ -0,0 +1,28 @@
+//Input :5
+//Output : 5 4 3 2 1
+
+#include<stdio.h>
+void Display(int iNo)
+{
+  int iCnt =0;
+
+  for(iCnt=0 ; iCnt<=iNo; iCnt++)
+  {
+    printf("%d\t",iCnt);
+  }
+  
+  
+
+}
+int main()
+{
+   int iValue = 0;
+   printf("Enter Number :\n");
+   scanf("%d",&iValue);
+
+   Display(iValue);
+
+
+    return 0;
+    
+}
